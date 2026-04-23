249810c9dda303b57132f385e07fd8597507b0d9
hupareshambhavi <shambhavihupare@gmail.com>
2026-04-23 15:46:10 +0200

    Day 1 Javascript Basics

---
+++ src/Javascript_basic/Day1.js
@@ -0,0 +1,35 @@
+console.log("Task 1");
+const name= "Shambhavi santosh Hupare";
+const university="Hochschule Fulda"
+let dream_company="Google"
+
+console.log("My name is: "+ name + "I study at: "+ university+ "My dream company is:"+dream_company);
+
+console.log("Task 2");
+
+function add(first_name,uni){
+    console.log("My name is:"+ first_name + "I study at:"+uni);
+}
+
+add("Shambhavi","Hochschule Fulda");
+
+
+console.log("Task 3")
+const multiply=(a,b)=>{ 
+    console.log(a*b);
+}
+multiply(5,2);
+
+
+console.log("Challenge");
+function age_calculator(age)
+{
+    if(age>18){
+        console.log("You ar an adult");
+    
+    }
+    else{
+        console.log("You are still a teenage");
+    }
+}
+age_calculator(20);
